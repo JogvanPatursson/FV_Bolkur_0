ed022c39768ec03cab89ba1f8c2f0d1e52ca5685
brbj1800 <brbj1800@student.miun.se>
2021-02-16 14:54:23 +0000

    Physics updated. Horizontal and Vertical speed added.

--- src/GameEngine/Physics.tsx
+++ src/GameEngine/Physics.tsx
@@ -1,17 +1,55 @@
-import React, { useState } from 'react';
-import ReactDOM from "react-dom";
 
+class PhysicsComponent {
+    public x : number;
+    public y : number;
+    public vspeed : number;
+    public hspeed : number;
+    public gravity : number;
+    
+    private currentGravity : number;
 
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+        this.vspeed = 0;
+        this.hspeed = 0;
+        this.gravity = 0;
+        this.currentGravity = 0;
+    }
 
-function PhysicsComponent(aX : number = 0, aY : number = 0,
-     aHspeed : number = 0, aVspeed : number = 0) {
+    public update() {
+        this.updateGravity();
+        
+        this.x += this.vspeed;
+        this.y += this.hspeed + this.currentGravity;
+    }
 
-    const [x, setX] = useState(aX);
-    const [y, setY] = useState(aY);
+    public setVspeed(vspeed : number) {
+        this.vspeed = vspeed;
+    }
 
+    public setHspeed(hspeed : number) {
+        this.hspeed = hspeed;
+    }
 
-    return [x, y];
+    public setX(x : number) {
+        this.x = x;
+    }
+
+    public setY(y : number) {
+        this.y = y;
+    }
+
+    public setGravity(gravity : number) {
+        this.gravity = gravity;
+    }
+
+    private updateGravity() {
+        if (this.currentGravity < this.gravity) {
+            this.currentGravity += 0.1;
+        }
+    }
+
 }
-
 
 export default PhysicsComponent;
