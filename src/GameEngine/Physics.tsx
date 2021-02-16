d7f6930714ff215384bd131755d4d90b01a2e53c
brbj1800 <brbj1800@student.miun.se>
2021-02-16 11:55:45 +0000

    Added physics

--- src/GameEngine/Physics.tsx
+++ src/GameEngine/Physics.tsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 
 
-function PhysicsComponent() {
-    
-    return <div></div>;
+
+function PhysicsComponent(aX : number = 0, aY : number = 0,
+     aHspeed : number = 0, aVspeed : number = 0) {
+
+    const [x, setX] = useState(aX);
+    const [y, setY] = useState(aY);
+
+
+    return [x, y];
 }
 
 
