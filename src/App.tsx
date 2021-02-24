7bcd565b20148a79f6a7bf2aa38619a8123fc1f7
Høgni Bøljer Wennerstrøm <hogniw@mail.fo>
2021-02-24 04:22:10 +0000

    added animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import TheBird from '../src/FlappyBird/TheBird';
-
+import { SpriteAnimator } from 'react-sprite-animator';
+import Flappy from './GameAssets/theGirl.png';
 
 const App: React.FC = () => {
-  
-    return TheBird();
-
+    return <SpriteAnimator sprite={Flappy} width={256} height={256} scale={2} fps={10} shouldAnimate={true}></SpriteAnimator>;
 };
 
 export default App;
+
