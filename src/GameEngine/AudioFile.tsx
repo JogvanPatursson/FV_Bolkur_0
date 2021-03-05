ad1972b661d022525629948908f49c86e7dd00ce
rhizashell <Haraldjac@gmail.com>
2021-03-05 13:22:44 +0000

    fixed the audiofile class

--- src/GameEngine/AudioFile.tsx
+++ src/GameEngine/AudioFile.tsx
@@ -1,41 +1,16 @@
-import { Howl, Howler} from "howler"
+
 export default class AudioFile {
 
     // placeholder for the sound variable
     private sound:any;
 
     // variables
-    private audioFileName:string;   // path to the audiofile "./audio.mp3"
-    private vol: any;               // the volume for the audio file
 
     // constructor
-    constructor(filename:string, pVol: number) {
-        this.audioFileName = filename;
-        this.sound = new Howl({
-            src: this.audioFileName,
-            autoplay: false,
-            volume: pVol,
-        });
+    constructor(filename:string) {
+        this.sound = new Audio(filename);
         // copying the value of pVol to vol
-        this.vol = pVol;
-
         //console.log("Audio File Created!")
-    }
-
-    // getters
-    public getFileName() : string {
-        return this.audioFileName;
-    }
-
-    // setters
-    public setVolume(pVol: number) : void {
-        if (pVol > 1.0 || pVol < 0.1 ) {
-            // console.log("Not a valid input!");
-        } else {
-            this.vol = pVol;
-            this.sound.volume(this.vol);
-            // console.log(`Volume has been set to ${this.vol}`)
-        }
     }
 
     // member functions
@@ -46,8 +21,7 @@ export default class AudioFile {
         //console.log("Playing audio!");
     }
 
-    // for looping the audio
-    public loopAudio() : void{
-        this.sound.loop = true;
+    public loopAudio() : void {
+        this.sound.loop();
     }
 }
