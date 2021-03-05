95b7fa0b75ec689aaf1a3d7a72b8daa76d1f75b2
Jogvan Patursson <joig1800@student.miun.se>
2021-03-05 10:11:04 +0000

    Update collision.test.tsx

--- src/GameEngine/unitTests/collision.test.tsx
+++ src/GameEngine/unitTests/collision.test.tsx
@@ -2,7 +2,28 @@ import React from 'react';
 import Physics from '../Physics';
 import Entity from '../Entity';
 import EntityList from '../EntityList';
+import Collision from '../Collision';
 
 test('collisionBoxCheck Test', () => {
+    // Arrange
+    const collisionObject = new Collision;
 
+
+    // Act
+
+    // Assert
+});
+
+test('collides test', () => {
+    // Arrange
+    const entityObject1 = new Entity(1, 'example', 0, 0, 10, 10);
+    const entityObject2 = new Entity(1, 'example', 0, 0, 10, 10);
+    const entityListObject = new EntityList();
+    entityListObject.pushArray(entityObject2);
+
+    // Act
+    entityObject1.collides(entityListObject);
+
+    // Assert
+    expect(entityObject1.collides(entityListObject)).toBe(true);
 });
